a349f9413cb56f69d930aa7dd85b274903c486df
VietnameseRick <95068784+VietnameseRick@users.noreply.github.com>
2025-10-21 23:37:46 +0700

    update address

--- abi/FHEPokerAddresses.ts
+++ abi/FHEPokerAddresses.ts
@@ -4,6 +4,6 @@
   By commands: 'npx hardhat deploy' or 'npx hardhat node'
 */
 export const FHEPokerAddresses = { 
-  "11155111": { address: "0x313F5083f59Cf3eBe8645293CBbbcd09415c6e63", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x7BA93614dDfD853e89f5fAcC55dF8cCbAde49561", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
 };
