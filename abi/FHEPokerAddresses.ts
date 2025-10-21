fb75d43f1a04bf6de92fe82a3caf0bdf0f8d6ab0
VietnameseRick <95068784+VietnameseRick@users.noreply.github.com>
2025-10-21 22:52:19 +0700

    update showdown & new contract ABI & CA

--- abi/FHEPokerAddresses.ts
+++ abi/FHEPokerAddresses.ts
@@ -4,6 +4,6 @@
   By commands: 'npx hardhat deploy' or 'npx hardhat node'
 */
 export const FHEPokerAddresses = { 
-  "11155111": { address: "0xe6E3E53A6A7D4aF9AeE0E90C3e4AD9cC02Df9b98", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x313F5083f59Cf3eBe8645293CBbbcd09415c6e63", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
 };
